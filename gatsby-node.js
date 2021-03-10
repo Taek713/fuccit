5ee74d84b8686b1b38395fecbf17d56a8e38e002
Tae Kim <tae.kim147@gmail.com>
2021-03-09 21:21:20 -0500

    Second commit fixing Gatsby build error from gatsby-node.js.

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,34 +5,34 @@
  */
 
 // You can delete this file if you're not using it
-const path = require(`path`)
+// const path = require(`path`)
 // Log out information after a build is done
-exports.onPostBuild = ({ reporter }) => {
-  reporter.info(`Your Gatsby site has been built!`)
-}
+// exports.onPostBuild = ({ reporter }) => {
+//   reporter.info(`Your Gatsby site has been built!`)
+// }
 // Create blog pages dynamically
-exports.createPages = async ({ graphql, actions }) => {
-  const { createPage } = actions
-  const blogPostTemplate = path.resolve(`src/templates/blog-post.js`)
-  const result = await graphql(`
-    query {
-      allSamplePages {
-        edges {
-          node {
-            slug
-            title
-          }
-        }
-      }
-    }
-  `)
-  result.data.allSamplePages.edges.forEach(edge => {
-    createPage({
-      path: `${edge.node.slug}`,
-      component: blogPostTemplate,
-      context: {
-        title: edge.node.title,
-      },
-    })
-  })
-}
+// exports.createPages = async ({ graphql, actions }) => {
+//   const { createPage } = actions
+//   const blogPostTemplate = path.resolve(`src/templates/blog-post.js`)
+//   const result = await graphql(`
+//     query {
+//       allSamplePages {
+//         edges {
+//           node {
+//             slug
+//             title
+//           }
+//         }
+//       }
+//     }
+//   `)
+//   result.data.allSamplePages.edges.forEach(edge => {
+//     createPage({
+//       path: `${edge.node.slug}`,
+//       component: blogPostTemplate,
+//       context: {
+//         title: edge.node.title,
+//       },
+//     })
+//   })
+// }
